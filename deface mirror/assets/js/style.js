d825bb876a56af69b3be1f2b35f7c0854e70f43f
kriz <anonymous.ip.ghost@gmail.com>
2017-02-04 03:28:31 +0700

    remove folder

--- deface mirror/assets/js/style.js
+++
@@ -1,1 +0,0 @@
-$("<select />").appendTo("nav");$("<option />",{"selected":"selected","value":"","text":"Go to..."}).appendTo("nav select");$("nav a").each(function(){var el=$(this);$("<option />",{"value":el.attr("href"),"text":el.text()}).appendTo("nav select")});$("nav select").change(function(){window.location=$(this).find("option:selected").val()});
